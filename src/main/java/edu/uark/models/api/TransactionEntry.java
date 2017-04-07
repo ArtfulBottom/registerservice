19d22a9ecd191f650b584d977c2b60e68a29db69
Perry Mills <pwm001@uark.edu>
2017-04-07 15:52:37 -0500

    finished TransactionEntry model and repository

--- src/main/java/edu/uark/models/api/TransactionEntry.java
+++ src/main/java/edu/uark/models/api/TransactionEntry.java
@@ -1,16 +1,13 @@
 package edu.uark.models.api;
 
-import java.time.LocalDateTime;
 import java.util.UUID;
+import java.math.BigDecimal;
 
 import org.apache.commons.lang3.StringUtils;
 
 import edu.uark.models.api.enums.TransactionApiRequestStatus;
-import edu.uark.models.entities.TransactionEntity;
-import edu.uark.models.entities.fieldnames.TransactionEntryFieldNames;
-import edu.uark.models.enums.TransactionClassification;
+import edu.uark.models.entities.TransactionEntryEntity;
 
-/*TODO update all fields within this class to match TransactionEntryEntity */
 public class TransactionEntry {
 	private UUID id;
 	public UUID getId() {
@@ -25,47 +22,37 @@ public class TransactionEntry {
     public UUID getTransactionId() {
     	return this.transactionId;
     }
-    public TransactionEntry setCashierId(UUID cashierId) {
-    	this.cashierId = cashierId;
+    public TransactionEntry setTransactionId(UUID transactionId) {
+    	this.transactionId = transactionId;
     	return this;
     }
     
     private UUID productId;
-    private int totalAmount;
-    public int getTotalAmount() {
-    	return this.totalAmount;
+    public UUID getProductId() {
+    	return this.productId;
     }
-    public TransactionEntry setTotalAmount(int totalAmount) {
-    	this.totalAmount = totalAmount;
+    public TransactionEntry setProductId(UUID productId) {
+    	this.productId = productId;
     	return this;
     }
     
-    private TransactionClassification classification;
-    public TransactionClassification getClassification() {
-    	return this.classification;
+    private int quantity;
+    public int getQuantity() {
+    	return this.quantity;
     }
-    public TransactionEntry setClassification(TransactionClassification classification) {
-    	this.classification = classification;
+    public TransactionEntry setQuantity(int quantity) {
+    	this.quantity = quantity;
     	return this;
     }
     
-    private LocalDateTime createdOn;
-	public LocalDateTime getCreatedOn() {
-		return this.createdOn;
-	}
-	public TransactionEntry setCreatedOn(LocalDateTime createdOn) {
-		this.createdOn = createdOn;
-		return this;
-	}
-	
-	private UUID referenceId;
-	public UUID getReferenceId() {
-		return this.referenceId;
-	}
-	public TransactionEntry setReferenceId(UUID referenceId) {
-		this.referenceId = referenceId;
-		return this;
-	}
+    private BigDecimal unitPrice;
+    public BigDecimal getUnitPrice() {
+    	return this.unitPrice;
+    }
+    public TransactionEntry setUnitPrice(BigDecimal unitPrice) {
+    	this.unitPrice = unitPrice;
+    	return this;
+    }
 	
 	private TransactionApiRequestStatus apiRequestStatus;
 	public TransactionApiRequestStatus getApiRequestStatus() {
@@ -92,26 +79,20 @@ public class TransactionEntry {
 	}
 	
 	public TransactionEntry() {
+		this.id = new UUID(0, 0);
 		this.transactionId = new UUID(0, 0);
-		this.totalAmount = 0;
-		this.classification = TransactionClassification.NOT_DEFINED;
-		this.createdOn = LocalDateTime.now();
-		this.referenceId = new UUID(0, 0);
+		this.productId = new UUID(0,0);
+		this.quantity = 0;
+		this.unitPrice = new BigDecimal("0.00");
 		this.apiRequestMessage = StringUtils.EMPTY;
-		this.apiRequestStatus = TransactionApiRequestStatus.OK;\
-		
-		this.transactionId = ((UUID) rs.getObject(TransactionEntryFieldNames.TRANSACTION_ID));
-		this.productId = ((UUID) rs.getObject(TransactionEntryFieldNames.PRODUCT_ID));
-		this.quantity = rs.getInt(TransactionEntryFieldNames.QUANTITY);
-		this.unitPrice = rs.getBigDecimal(TransactionEntryFieldNames.UNIT_PRICE);
+		this.apiRequestStatus = TransactionApiRequestStatus.OK;
 	}
 	
-	public TransactionEntry(TransactionEntity transactionEntity) {
-		this.cashierId = transactionEntity.getCashierId();
-		this.totalAmount = transactionEntity.getTotalAmount();
-		this.classification = transactionEntity.getClassification();
-		this.createdOn = transactionEntity.getCreatedOn();
-		this.referenceId = transactionEntity.getReferenceId();
+	public TransactionEntry(TransactionEntryEntity transactionEntryEntity) {
+		this.id = transactionEntryEntity.getTransactionId();
+		this.productId = transactionEntryEntity.getProductId();
+		this.quantity = transactionEntryEntity.getQuantity();
+		this.unitPrice = transactionEntryEntity.getUnitPrice();
 
 		this.apiRequestMessage = StringUtils.EMPTY;
 		this.apiRequestStatus = TransactionApiRequestStatus.OK;
--- src/main/java/edu/uark/models/entities/TransactionEntryEntity.java
+++ src/main/java/edu/uark/models/entities/TransactionEntryEntity.java
@@ -2,15 +2,13 @@ package edu.uark.models.entities;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.UUID;
+import java.math.BigDecimal;
 
 import edu.uark.dataaccess.entities.BaseEntity;
 import edu.uark.models.api.TransactionEntry;
 import edu.uark.models.entities.fieldnames.TransactionEntryFieldNames;
-import edu.uark.models.enums.TransactionClassification;
 import edu.uark.models.repositories.TransactionEntryRepository;
 
 public class TransactionEntryEntity extends BaseEntity<TransactionEntryEntity> {
@@ -24,112 +22,99 @@ public class TransactionEntryEntity extends BaseEntity<TransactionEntryEntity> {
 
 	@Override
 	protected Map<String, Object> fillRecord(Map<String, Object> record) {
-		record.put(TransactionFieldNames.CASHIER_ID, this.cashierId);
-		record.put(TransactionFieldNames.TOTAL_AMOUNT, this.totalAmount);
-		record.put(TransactionFieldNames.CLASSIFICATION, this.classification.getValue());
-		record.put(TransactionFieldNames.CREATED_ON, Timestamp.valueOf(this.createdOn));
-		record.put(TransactionFieldNames.REFERENCE_ID, this.referenceId);
+		record.put(TransactionEntryFieldNames.TRANSACTION_ID, this.transactionId);
+		record.put(TransactionEntryFieldNames.PRODUCT_ID, this.productId);
+		record.put(TransactionEntryFieldNames.QUANTITY, this.quantity);
+		record.put(TransactionEntryFieldNames.UNIT_PRICE, this.unitPrice);
 		
 		return record;
 	}
     
-    private UUID cashierId;
-    public UUID getCashierId() {
-    	return this.cashierId;
+    private UUID transactionId;
+    public UUID getTransactionId() {
+    	return this.transactionId;
     }
-    public TransactionEntryEntity setCashierId(UUID cashierId) {
-    	if (!this.cashierId.equals(cashierId)) {
-    		this.cashierId = cashierId;
-    		this.propertyChanged(TransactionFieldNames.CASHIER_ID);
+    public TransactionEntryEntity setTransactionId(UUID transactionId) {
+    	if (!this.transactionId.equals(transactionId)) {
+    		this.transactionId = transactionId;
+    		this.propertyChanged(TransactionEntryFieldNames.TRANSACTION_ID);
     	}
     	
     	return this;
     }
     
-    private int totalAmount;
-    public int getTotalAmount() {
-    	return this.totalAmount;
+    private UUID productId;
+    public UUID getProductId() {
+    	return this.productId;
     }
-    public TransactionEntryEntity setTotalAmount(int totalAmount) {
-    	if (this.totalAmount != totalAmount) {
-    		this.totalAmount = totalAmount;
-    		this.propertyChanged(TransactionFieldNames.TOTAL_AMOUNT);
+    public TransactionEntryEntity setProductId(UUID productId) {
+    	if (!this.productId.equals(transactionId)) {
+    		this.productId = transactionId;
+    		this.propertyChanged(TransactionEntryFieldNames.PRODUCT_ID);
     	}
     	
     	return this;
     }
     
-    
-    private TransactionClassification classification;
-    public TransactionClassification getClassification() {
-    	return this.classification;
+    private int quantity;
+    public int getQuantity() {
+    	return this.quantity;
     }
-    public TransactionEntryEntity setClassification(TransactionClassification classification) {
-    	if (this.classification != classification) {
-    		this.classification = classification;
-    		this.propertyChanged(TransactionFieldNames.CLASSIFICATION);
+    public TransactionEntryEntity setQuantity(int quantity) {
+    	if (this.quantity != quantity) {
+    		this.quantity = quantity;
+    		this.propertyChanged(TransactionEntryFieldNames.QUANTITY);
     	}
     	
     	return this;
     }
     
-    private LocalDateTime createdOn;
-	public LocalDateTime getCreatedOn() {
-		return this.createdOn;
-	}
+    private BigDecimal unitPrice;
+    public BigDecimal getUnitPrice() {
+    	return this.unitPrice;
+    }
+    public TransactionEntryEntity setUnitPrice(BigDecimal unitPrice) {
+    	if (this.unitPrice != unitPrice) {
+    		this.unitPrice = unitPrice;
+    		this.propertyChanged(TransactionEntryFieldNames.UNIT_PRICE);
+    	}
+    	
+    	return this;
+    }
 	
-	private UUID referenceId;
-	public UUID getReferenceId() {
-		return this.referenceId;
-	}
-	public TransactionEntryEntity setReferenceId(UUID referenceId) {
-		if (!this.referenceId.equals(referenceId)) {
-			this.referenceId = referenceId;
-			this.propertyChanged(TransactionFieldNames.REFERENCE_ID);
-		}
+	public TransactionEntry synchronize(TransactionEntry apiTransactionEntry) {
+		this.setTransactionId(apiTransactionEntry.getTransactionId());
+		this.setProductId(apiTransactionEntry.getProductId());
+		this.setQuantity(apiTransactionEntry.getQuantity());
+		this.setUnitPrice(apiTransactionEntry.getUnitPrice());
 		
-		return this;
-	}
-	
-	public Transaction synchronize(Transaction apiTransaction) {
-		this.setCashierId(apiTransaction.getCashierId());
-		this.setTotalAmount(apiTransaction.getTotalAmount());
-		this.setClassification(apiTransaction.getClassification());
-		this.setReferenceId(apiTransaction.getReferenceId());
-		
-		apiTransaction.setCreatedOn(this.createdOn);
-		
-		return apiTransaction;
+		return apiTransactionEntry;
 	}
 	
 	public TransactionEntryEntity() {
 		super(new TransactionEntryRepository());
 		
-		this.cashierId = new UUID(0, 0);
-		this.totalAmount = 0;
-		this.classification = TransactionClassification.NOT_DEFINED;
-		this.createdOn = LocalDateTime.now();
-		this.referenceId = new UUID(0, 0);
+		this.transactionId = new UUID(0, 0);
+		this.productId = new UUID(0, 0);
+		this.quantity = 0;
+		this.unitPrice = new BigDecimal("0.00");
 	}
 	
 	public TransactionEntryEntity(UUID id) {
 		super(id, new TransactionEntryRepository());
 		
-		this.cashierId = new UUID(0, 0);
-		this.totalAmount = 0;
-		this.classification = TransactionClassification.NOT_DEFINED;
-		this.createdOn = LocalDateTime.now();
-		this.referenceId = new UUID(0, 0);
+		this.transactionId = new UUID(0, 0);
+		this.productId = new UUID(0, 0);
+		this.quantity = 0;
+		this.unitPrice = new BigDecimal("0.00");
 	}
 
-	public TransactionEntryEntity(Transaction apiTransaction) {
-		super(apiTransaction.getId(), new TransactionEntryRepository());
+	public TransactionEntryEntity(TransactionEntry apiTransactionEntry) {
+		super(apiTransactionEntry.getId(), new TransactionEntryRepository());
 		
-		this.cashierId = apiTransaction.getCashierId();
-		this.totalAmount = apiTransaction.getTotalAmount();
-		this.classification = apiTransaction.getClassification();
-		this.referenceId = apiTransaction.getReferenceId();
-
-		this.createdOn = LocalDateTime.now();
+		this.setTransactionId(apiTransactionEntry.getTransactionId());
+		this.setProductId(apiTransactionEntry.getProductId());
+		this.setQuantity(apiTransactionEntry.getQuantity());
+		this.setUnitPrice(apiTransactionEntry.getUnitPrice());
 	}
 }
